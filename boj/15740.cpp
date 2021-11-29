ec6fa9f88edde200ce8d4ac21c2ef18252df1f62
root <root@N.localdomain>
2021-11-29 17:35:55 +0900

    211129

--- boj/15740.cpp
+++ boj/15740.cpp
@@ -1,142 +1,111 @@
-#include <iostream>
-#include <string>
+#include <cstdio>
 
-using namespace std;
+#define SIZE 10001
 
-//na, nb : a와 b를 각 자리수별로 나누어 저장
-int na[10001], nb[10001];
+void swap(char &a, char &b) {
+    char temp = a;
+    a = b;
+    b = temp;
+}
 
-int main()
-{
-	//cin, cout 사용 시 필히 사용할 것
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+// 큰 수를 클래스에 넣어서 저장
+class Number {
+public:
+    // 이 클래스에 저장된 수가 음수인지를 prefix하여 저장
+    bool is_minus = false;
+    // 입력된 수를 각 자리수별로 나누어서 저장
+    char dgt[SIZE] = { 0, };
 
-	//두 수 a, b를 차례로 입력받는다
-	string a, b;
-	cin >> a >> b;
+    // 수를 입력받으며 값을 저장하는 생성자
+    Number() {
+        // 각 자리를 입력할 때 사용할 공간
+        char buf;
+        // l: 수를 입력받을 때 사용할 인덱스
+        // r: 입력받은 수는 역순으로 저장되므로 
+        // 투포인터를 이용해 수를 뒤집을 때 쓸 인덱스
+        int l = SIZE - 1, r = l;
 
-	//a가 음수라면 이를 표시하고 a를 양수처럼 사용한다
-	int index_a = 0, isMin_a = 1;
-	if (a[0] == '-') isMin_a = -1;
+        // while문을 이용해 수를 입력받는다
+        scanf("%c", &buf);
+        // 버퍼에 공백 문자가 나올 때까지
+        while (buf != ' ' && buf != '\n') {
+            // 만일 - 기호가 들어왔다면 이 수가 음수임을 표시한다
+            if (buf == '-') this->is_minus = true;
+            // - 기호 이외에 들어온 값, 즉 각 자리 수를 역순으로 배열에 저장
+            else this->dgt[l--] = buf - '0';
+            // 다음 문자를 입력받는다
+            scanf("%c", &buf);
+        }
 
-	//양수 a를 각 자리수별로 나누어 na에 저장
-	//a가 사실 음수라면 각 자리수에 -1을 곱해 음수임을 표시
-	while (!(a.empty()) && !((a.size() == 1) && (a[0] == '-')))
-	{
-		na[index_a] = atoi(&a[a.size() - 1]) * isMin_a;
-		a.pop_back();
-		index_a += 1;
-	}
+        // 역순으로 저장된 수를 다시 뒤집는다
+        while (l < r) swap(this->dgt[++l], this->dgt[r--]);
+        // 이 수가 음수라면 10의 보수 기법을 이용해 양수로 저장
+        if (this->is_minus) this->complement();
+    }
 
-	//b가 음수라면 이를 표시하고 b를 양수처럼 사용한다
-	int index_b = 0, isMin_b = 1;
-	if (b[0] == '-') isMin_b = -1;
+    // 수를 입력받지 않고 값을 1로 설정
+    Number(int i) { this->dgt[SIZE - 1] = 1; }
 
-	//양수 b를 각 자리수별로 나누어 nb에 저장
-	//b가 사실 음수라면 각 자리수에 -1을 곱해 음수임을 표시
-	while ((!b.empty()) && !((b.size() == 1) && (b[0] == '-')))
-	{
-		nb[index_b] = atoi(&b[b.size() - 1]) * isMin_b;
-		b.pop_back();
-		index_b += 1;
-	}
+    // 수 x를 10의 보수를 이용하여 절댓값을 바꾸지 않으면서 부호를 전환
+    void complement() {
+        // 각 자리 수 d를 9 - d로 변환하여 x를 ~x로 변환
+        for (int i = 0; i < SIZE; i++) this->dgt[i] = 9 - this->dgt[i];
 
-	//a와 b중 자리수가 더 큰 수의 자리수를 저장
-	int size = (index_a > index_b) ? index_a : index_b;
+        // ~x에 1을 더해 -x를 완성시킨다
+        this->add(new Number(1));
+    }
 
-	//b의 자리수가 a보다 크다면
-	if (index_b > index_a)
-	{
-		//a와 b를 스왑하여 a의 절댓값이 b의 절댓값보다 크게 한다
-		swap(na, nb);
-		swap(index_a, index_b);
-		swap(isMin_a, isMin_b);
-	}
+    // 현재 수에 other를 더한다
+    void add(Number* other) {
+        // carry: 각 자리 덧셈에서 발생한 carry
+        // temp_minus: 수의 범위가 ±10^10000을 포함하므로
+        // 예외처리를 위해 각 자리 수를 검토하는 변수
+        bool carry = false, temp_minus = false;
 
-	//a의 자리수가 b와 같다면
-	else if (index_b == index_a)
-	{
-		//a와 b의 각 자리 수를 비교하여 a와 b의 대소 판단
-		int cond;
+        // 각 자리의 덧셈
+        for (int i = SIZE - 1; i >= 0; i--) {
+            // 두 수의 i번째 자리를 직전 수의 carry와 함께 더한 뒤
+            this->dgt[i] += other->dgt[i] + carry;
+            // 덧셈에서 발생한 carry를 따로 저장하고
+            carry = this->dgt[i] / 10;
+            // 나며지 연산으로 각 자리 수를 저장
+            this->dgt[i] %= 10;
 
-		for (int i = size - 1; i >= 0; i--)
-		{
-			//na의 i자리 수가 nb의 i자리 수보다 작다면 음수, 크다면 양수
-			cond = na[i] * isMin_a - nb[i] * isMin_b;
+            // 가장 큰 자리가 아닌 자리의 수가 0이 아니라면 temp_minus를 true로 설정
+            if (i != 0) temp_minus = temp_minus || this->dgt[i];
+        }
 
-			if (na[i] * isMin_a < nb[i] * isMin_b)
-			{
-				//a와 b를 스왑하여 a의 절댓값이 b의 절댓값보다 크게 한다
-				swap(na, nb);
-				swap(index_a, index_b);
-				swap(isMin_a, isMin_b);
+        // 가장 큰 자리의 수가 2 초과거나, 2이면서 temp_minus라면
+        // 이 덧셈의 결과는 음수이므로 이를 is_minus에 저장
+        this->is_minus = (this->dgt[0] > 2) || (this->dgt[0] == 2 && temp_minus);
+    }
+};
 
-				break;
-			}
+int main() {
+    // 출력을 위해 각 자리가 0인지 확인
+    bool chk = false;
+    // 두 수 a, b를 생성자를 통해 입력받아 저장
+    Number a, b;
+    // a += b
+    a.add(&b);
 
-			else if (na[i] * isMin_a > nb[i] * isMin_b) break;
-		}
-	}
+    // a가 음수라면 - 기호를 출력한 뒤 a에 10의 보수를 취해 절댓값을 복원
+    if (a.is_minus) {
+        printf("-");
+        a.complement();
+    }
+    // a의 각 자리의 수 a'에 대해
+    for (int i = 0; i < SIZE; i++) {
+        // 출력 앞에 자리만 차지하는 0이 있어서는 안되므로
+        // a를 탐색하며 처음 0이 아닌 수를 발견했을 때부터 a를 출력
+        chk = chk || a.dgt[i];
+        // 0이 아닌 수를 발견한 적이 있거나 i가 SIZE - 1일 때, 즉 a의 값이 0일 때
+        // a의 각 자리 수 a'를 출력
+        if (chk || i == SIZE - 1) printf("%d", a.dgt[i]);
+    }
 
-	//a가 음수라면 a와 b에 -1을 곱해 결과가 양수가 나오게 한다
-	//a가 양수고 b가 음수라면 결과는 반드시 양수가 나온다
-	if (isMin_a == -1)
-	{
-		for (int i = 0; i < index_a; i++) na[i] *= -1;
-		for (int i = 0; i < index_b; i++) nb[i] *= -1;
-	}
-
-	//a와 b의 각 자리수를 더한다
-	for (int i = 0; i < size; i++)
-	{
-		na[i] += nb[i];
-
-		//자리수의 합이 음수라면 다음 자리수에서 값을 가져온다
-		while (na[i] < 0)
-		{
-			na[i] += 10;
-			na[i + 1] -= 1;
-		}
-		//자리수의 합이 10 이상이라면 다음 자리수에 값을 보낸다
-		while (na[i] >= 10)
-		{
-			na[i] -= 10;
-			na[i + 1] += 1;
-		}
-	}
-
-	//덧셈 결과 자리수가 늘었다면 그만큼 size를 늘려준다
-	while (size <= 10000)
-	{
-		if (na[size] != 0)
-		{
-			while (na[size] >= 10)
-			{
-				//늘어난 자리의 자리수가 10 이상이라면 다음 자리수에 값을 보낸다
-				na[size] -= 10;
-				na[size + 1] += 1;
-			}
-
-			size += 1;
-		}
-		else break;
-	}
-
-	//덧셈 결과 자리수가 줄었다면 그만큼 size를 줄여준다
-	while (size > 1)
-	{
-		if (na[size - 1] == 0) size -= 1;
-		else break;
-	}
-
-	//a가 음수였다면 a에 -1을 곱해 다시 음수로 만든다
-	if (isMin_a == -1) na[size - 1] *= -1;
-
-	//a 출력
-	for (int i = size - 1; i >= 0; i--) cout << na[i];
-	cout << '\n';
+    // 개행 문자를 출력하여 출력을 종료
+    printf("\n");
 
     return 0;
 }
