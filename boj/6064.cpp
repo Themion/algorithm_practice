1bd8133dbaea91891369d7204120f89f04614056
themion <themion@naver.com>
2021-12-24 13:59:39 +0900

    refactoring

--- boj/6064.cpp
+++ boj/6064.cpp
@@ -1,27 +1,34 @@
 #include <cstdio>
 
-int main()
-{
-	int tc, M, N, x, y;
-	scanf("%d", &tc);
-	while (tc--)
-	{
-		int a, b, cnt;
-		bool rec[40001] = { false, };
-		scanf("%d %d %d %d", &M, &N, &x, &y);
+#define MAX_N 40000
 
-		a = (y - 1) % M + 1; b = y; cnt = y - N;
+int test_case() {
+    // visit[x_]: 카잉 년도 <x_, y>를 발견했다면 true
+    bool visit[MAX_N] = { false, };
+    // M, N: 카잉 년도의 주기, <x, y>: 찾고자 하는 년도의 카잉 년도
+    // ans: 입력받은 카잉 년도를 변환한 값
+    int M, N, x, y, ans;
 
-		do
-		{
-			rec[a] = true;
-			cnt += N;
-			a = (a - 1 + N) % M + 1;
-		} while (!rec[a] && !rec[x]);
+    // 문제의 조건을 입력받은 뒤
+    scanf("%d %d %d %d", &M, &N, &x, &y);
 
-		if (!rec[x]) cnt = -1;
-		printf("%d\n", cnt);
-	}
+    // 계산의 편의를 위해 x와 y에 값을 1씩 차감
+    // 임의의 년도 <x_, y_>에 대해 y_ = y를 만족하는 최소의 ans를 계산
+    x--; ans = --y;
+    
+    // 년도를 N씩 늘리며 카잉 년도 <x, y>를 만족하는 경우가 있는지 확인
+    for (; !visit[x] && !visit[ans % M]; ans += N)
+        visit[ans % M] = true;
 
-	return 0;
+    // 카잉 년도 <x, y>를 만족하는 경우가 있다면 그 년도를, 없다면 -1을 반환
+    // ans를 반환할 경우, 년도의 시작이 1이고 for문에서 x를 표시한 뒤 N을 더했으므로
+    // ans가 아닌 ans - N + 1을 반환해야 한다
+    return visit[x] ? ans - N + 1 : -1;
+}
+
+int main() {
+    int T;
+    // 테스트 케이스를 입력받고 각 테스트 케이스의 정답을 출력
+    for (scanf("%d", &T); T--; ) printf("%d\n", test_case());
+    return 0;
 }
--- boj/6603.cpp
+++ boj/6603.cpp
@@ -1,49 +1,35 @@
 #include <cstdio>
 
-//arr: 로또 번호 후보를 저장할 공간
-//picks: 로또 번호 후보 중 로또로 예측되는 번호
-int arr[13], picks[6];
+#define MIN_K 6
+#define MAX_K 12
 
-//현재 idx개의 번호를 뽑았으며 마지막으로 뽑은 번호는 last번째 번호이다
-void pick(int idx, int last)
-{
-    //6개 미만의 번호를 뽑았다면
-    if(idx < 6)
-    {
-        //last + 1번째 번호부터 실질적으로 가능한 번호들을 전부 뽑는다
-        for(int i = last + 1; i + (5 - idx) <= arr[0]; i++)
-        {
-            //각 번호는 idx + 1번째 번호가 되고
-            picks[idx] = arr[i];
-            //이후 pick함수를 재귀적으로 수행한다
-            pick(idx + 1, i);
-        }
+// len: 로또 번호의 개수, arr: 각 로또 번호
+// picks: 로또 번호 후보 중 로또로 예측되는 번호
+int len, arr[MAX_K], picks[MIN_K];
+
+// 현재 idx개의 번호를 뽑았으며 마지막으로 뽑은 번호는 last번째 번호이다
+void pick(int idx, int last) {
+    // 번호를 전부 뽑지 않았다면
+    // last 이후 가능한 번호들을 전부 뽑은 뒤 다음 번호를 고른다
+    if (idx < MIN_K) for (int i = last + 1; i < len; i++) {
+        picks[idx] = arr[i];
+        pick(idx + 1, i);
     }
-    //만일 6개의 번호를 모두 뽑았다면
-    else
-    {
-        //각 번호를 순서대로 출력한뒤 행을 바꾼다
-        for (int i = 0; i <= 5; i++) printf("%d ", picks[i]);
-        printf("\n");
-    }
+    // 6개의 번호를 모두 뽑았다면 각 번호를 순서대로 출력한뒤 행을 바꾼다
+    else for (int i = 0; i < MIN_K; i++) 
+        printf("%d%c", picks[i], (i == MIN_K - 1 ? '\n' : ' '));
 }
 
-int main()
-{
-    //arr[0]을 로또 번호 후보의 수로 사용한다
-    scanf("%d", &arr[0]);
+void test_case() {
+    // 각 로또 번호를 입력받은 뒤
+    for (int i = 0; i < len; i++) scanf("%d", arr + i);
+    // 가능한 경우를 모두 출력한 뒤 줄바꿈을 출력해 테스트 케이스의 출력을 종료
+    pick(0, -1);
+    printf("\n");
+}
 
-    //테스트 케이스의 길이가 0이 아니라면
-    while(arr[0] != 0)
-    {
-        //테스트 케이스를 입력받은 뒤
-        for (int i = 1; i <= arr[0]; i++) scanf("%d", &arr[i]);
-        //로또 번호를 처음부터 유추한다
-        pick(0, 0);
-        //테스트 케이스가 끝났으므로 개행문자를 통해 테스트 케이스의 끝을 알린다
-        printf("\n");
-
-        //다음 테스트 케이스의 길이를 입력받는다
-        scanf("%d", &arr[0]);
-    }
+int main() {
+    // 길이가 0이 아닌 모든 테스트 케이스에 대해 가능한 경우를 출력
+    while (scanf("%d", &len) && len) test_case();
+    return 0;
 }
