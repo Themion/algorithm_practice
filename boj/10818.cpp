564f8800feea6d2875c0db0673b05e66a117795f
themion <themion@naver.com>
2021-12-30 17:35:36 +0900

    refactoring

--- boj/10818.cpp
+++ boj/10818.cpp
@@ -1,18 +1,27 @@
-#include <cstdio>
+#include <iostream>
 
-int main()
-{
-    int n, buf, min = 1000000, max = -1000000;
-    
-    scanf("%d", &n);
-    while(n--)
-    {
-        scanf("%d", &buf);
-        if(min > buf) min = buf;
-        if(max < buf) max = buf;
+using namespace std;
+
+#define MAX 1000000
+
+int main() {
+    // 입출력 속도 향상
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    // N: 입력받을 정수의 개수, i: 입력받을 각 정수, min, max: 입력의 최솟값 / 최댓값
+    int N, i, min = MAX, max = -MAX;
+
+    // 각 정수를 입력받은 뒤 최솟값 / 최댓값과 비교해 갱신
+    for (cin >> N; N--; ) {
+        cin >> i;
+        if (min > i) min = i;
+        if (max < i) max = i;
     }
 
-    printf("%d %d", min, max);
+    // 최솟값과 최댓값을 각각 출력
+    cout << min << ' ' << max << '\n';
 
     return 0;
 }
